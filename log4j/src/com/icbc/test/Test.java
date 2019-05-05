e4c55095d7560d6dde72d04963ce447c2358c65e
'Neptune8442' <'kenwisdom420@163.com'>
2019-05-05 21:49:27 +0800

    test2

--- log4j/src/com/icbc/test/Test.java
+++ log4j/src/com/icbc/test/Test.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger;
 public class Test {
 	public static void main(String[] args) {
 		Logger logger =Logger.getLogger(Test.class); 
-		logger.debug("这是一个调试信息");
+		logger.debug("这是一个调试信息"); 
 		logger.info("普通信息");
 		
 		try {
